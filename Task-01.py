bff65beda85cea09dcad2c1f23348ca40feeab15
rtretyakov <rtretyakov@outlook.com>
2020-12-16 21:51:16 +1000

    Tasks 1 - 4 completed

--- Task-01.py
+++ Task-01.py
@@ -3,3 +3,44 @@
 # Урок 3. Задание 1:
 #  Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
 #  Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль
+
+def divide_num(*args):
+
+    return args[0]/args[1]
+
+
+def get_num(order_num):
+
+    while True:
+        input_num = input(f'Введите {order_num} число, либо пустое значение для прерывания ввода: ')
+        if input_num == '':
+            break
+
+        try:
+            input_num = float(input_num)
+        except ValueError:
+            print('Ошибка при преобразовании введенного значения в число! Повторите ввод')
+        else:
+            break
+
+    return input_num
+
+
+while True:
+    first_num = get_num(1)
+    if first_num == '':
+        break
+
+    second_num = get_num(2)
+    if second_num == '':
+        break
+
+    try:
+        divide_res = divide_num(first_num, second_num)
+    except ZeroDivisionError:
+        print('Ошибка деления на ноль!')
+        continue
+    else:
+        print(f'Результат операции {first_num} : {second_num} = {divide_res}')
+
+print('Операция отменена!')
--- Task-02.py
+++ Task-02.py
@@ -4,3 +4,29 @@
 # Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя: имя,
 # фамилия, год рождения, город проживания, email, телефон. Функция должна принимать параметры
 # как именованные аргументы. Реализовать вывод данных о пользователе одной строкой
+
+def define_user(first_name='', last_name='', birth_year='', city='', email='', phone_num=''):
+
+    if first_name:
+        print('Имя:', first_name, end = '; ')
+
+    if last_name:
+        print('Фамилия:', last_name, end = '; ')
+
+    if birth_year:
+        print('Год рождения:', birth_year, end = '; ')
+
+    if city:
+        print('Город проживания:', city, end = '; ')
+
+    if email:
+        print('e-mail:', email, end = '; ')
+
+    if phone_num:
+        print('№ телефона:', phone_num, end = '; ')
+
+    print('\n')
+
+define_user(first_name = 'Roman', last_name = 'Tretyakov', birth_year = 1975)
+define_user(first_name = 'Roman', city = 'Ussuriisk', email = 'rtretyakov@outlook.com')
+define_user(first_name = 'Roman', last_name = 'Tretyakov')
